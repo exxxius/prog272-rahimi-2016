dc414f04048c6d8fbda0af6c3f0ad6aa2750355e
exxius <mehdi.rahimi@bellevuecollege.edu>
2017-04-16 16:42:29 -0700

    First commit

---
+++ week02-react-jest/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+      super();
+      this.state = {
+          nine: '0'
+      }
+  }
+getNine = () => {
+   console.log('state');
+   this.setState({nine: '9'})
+}
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+	<p className="App-intro">Nine: {this.state.nine}</p>
+        <button className='elf' onClick={this.getNine}>Get Nine</button>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ week02-react-jest/src/App.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { shallow } from 'enzyme';
+
+describe('jest test', function() {
+
+   it('renders without crashing', () => {
+       const div = document.createElement('div');
+       ReactDOM.render(<App />, div);
+   });
+
+   it('renders and reads H1 text', () => {
+       const wrapper = shallow(<App />);
+       const welcome = <h2>Welcome to React</h2>;
+       expect(wrapper.contains(welcome)).toEqual(true);
+   });
+  it('renders button click message', () => {
+     const wrapper = shallow(<App />);
+     const nineSign = <p className="App-intro">Nine: 9</p>;
+     wrapper.find('button.elf').simulate('click');
+     expect(wrapper.contains(nineSign)).toEqual(true);
+  });
+});
