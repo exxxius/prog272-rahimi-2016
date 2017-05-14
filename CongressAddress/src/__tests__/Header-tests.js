f7feb1218157d7cd2d72369bedb07eb1f6bacbbf
Charlie on RohanElf <charlie@elvenware.com>
2017-05-13 19:27:06 -0700

    CharlieComments on midterm

--- CongressAddress/src/__tests__/Header-tests.js
+++ CongressAddress/src/__tests__/Header-tests.js
@@ -5,11 +5,15 @@ import React from 'react';
 import Header from '../components/ElfHeader';
 import {shallow} from 'enzyme';
 
+import ElfEnzymeDebug from '../ElfEnzymeDebug.js';
+const elfDebug = new ElfEnzymeDebug(true, 'AddressChanger.test.js');
+
 describe('Header test', function () {
 
     it('renders and reads H2 text', () => {
         const wrapper = shallow(<Header />);
-        const welcome = <h2>Welcome to React</h2>;
+        const welcome = <h2>Welcome to Congress Address List</h2>;
+        elfDebug.getAll(wrapper, 'div');
         expect(wrapper.contains(welcome)).toEqual(true);
     });
 
